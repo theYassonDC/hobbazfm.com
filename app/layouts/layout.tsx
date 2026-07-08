c47bada2bd8f49cb7e583c20a28ab1e32339360c
theYassonDC <yassonword@gmail.com>
2026-07-08 01:17:56 -0500

    update meta description

--- app/layouts/layout.tsx
+++ app/layouts/layout.tsx
@@ -6,7 +6,7 @@ import type { Route } from "./+types/layout";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "HobbazFM ~ Tu mejor radio" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Bienvenido a tu radio favorita de hobbaz.es!" },
   ];
 }
 
